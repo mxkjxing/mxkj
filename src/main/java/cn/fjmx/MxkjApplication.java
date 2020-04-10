25210efd4d5438389f06482b42ab56a4d47d1b68
xingwei <1454972037@qq.com>
2020-04-10 11:31:26 +0800

    上传项目

---
+++ src/main/java/cn/fjmx/MxkjApplication.java
@@ -0,0 +1,13 @@
+package cn.fjmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxkjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxkjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/fjmx/MxkjApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fjmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxkjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
